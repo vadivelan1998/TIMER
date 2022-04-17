af922cd76a8fd243d21e1ba474e671266b7f813b
vadivelan1998 <97453410+vadivelan1998@users.noreply.github.com>
2022-04-17 21:47:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Timer from "./components/timer.jsx"
+
+function App() {
+  return (
+    <div className="App">
+       <Timer/>
+    </div>
+  );
+}
+
+export default App;
